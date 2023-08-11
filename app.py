cc3d7045710873e44439fe1a7b12c0aa926950d4
shaarang.pd <84028033+shaarang12@users.noreply.github.com>
2023-08-11 18:27:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,9 @@
-# import streamlit as st
-# import numpy as np
-# import tensorflow as tf
-# from tensorflow.keras.applications import resnet as rt
-# from PIL import Image
-# import pickle
-
-# # Create an instance of the model architecture
-# model = tf.keras.applications.ResNet50(weights=None, include_top=True, classes=1000)
-
-# # Load the saved model weights using pickle
-# with open('modelRESNET.pkl', 'rb') as f:
-#     model_weights = pickle.load(f)
-#     model.set_weights(model_weights)
-
-
-
-# def preprocess_image(image):
-#     image = image.resize((224, 224))  # Resize image to match model input size
-#     image = np.array(image) / 255.0  # Normalize pixel values to [0, 1]
-#     image = np.expand_dims(image, axis=0)  # Add batch dimension
-#     return image
-
-
-
-# def main():
-#     st.title("Image Classification App")
-#     st.write("Upload an image for prediction")
-
-#     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
-#     if uploaded_file is not None:
-#         image = Image.open(uploaded_file)
-#         st.image(image, caption='Uploaded Image.', use_column_width=True)
-
-#         if st.button("Predict"):
-#             # Preprocess the image
-#             input_image = preprocess_image(image)
-
-#             processed_image = rt.preprocess_input(input_image.copy())
-
-#             # Perform prediction
-#             predictions = model.predict(processed_image)
-#             predicted_class = np.argmax(predictions[0])
-
-#             st.write(f"Predicted class: {class_labels[predicted_class]}")
-
-# if __name__ == "__main__":
-#     main()
-
-
-
-
-
 import streamlit as st
-#tensor
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.applications import resnet as rt
 from PIL import Image
 from keras.models import load_model
-from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.applications.resnet import preprocess_input
 
 # Load the saved model
 model = load_model('ModelWeights.h5')
@@ -69,9 +13,9 @@ class_labels = ['ACA', 'N', 'SCC']  # Replace with your actual class labels
 
 def preprocess_image(image):
     image = image.resize((224, 224))  # Resize image to match model input size
-    imagen = img_to_array(image)  # Normalize pixel values to [0, 1]
-    imageb = np.expand_dims(imagen, axis=0)  # Add batch dimension
-    return imageb.copy()
+    image_array = np.array(image)
+    processed_image = preprocess_input(image_array)  # Preprocess image using ResNet preprocessing
+    return processed_image
 
 def main():
     st.title("Image Classification App")
@@ -86,18 +30,14 @@ def main():
         if st.button("Predict"):
             # Preprocess the image
             input_image = preprocess_image(image)
-            processed_image = rt.preprocess_input(input_image)
+            input_batch = np.expand_dims(input_image, axis=0)
+
             # Perform prediction
-            predictions = model.predict(processed_image)
-            st.write(predictions)
-            # predicted_class = np.argmax(predictions, axis = 1)
-            # #Adenocarcinoma, Normal, Squamous Cell Carcinoma
-            # if(predicted_class == [0]):
-            #     st.write(f"Predicted class: Adenocarcinoma")
-            # elif(predicted_class == [1]):
-            #     st.write(f"Predicted class: Benign Tissue")
-            # else:
-            #     st.write(f"Predicted class: Squamous Cell Carcinoma")
+            predictions = model.predict(input_batch)
+            predicted_class_idx = np.argmax(predictions[0])
+            predicted_class = class_labels[predicted_class_idx]
+
+            st.write(f"Predicted class: {predicted_class}")
 
 if __name__ == "__main__":
     main()
